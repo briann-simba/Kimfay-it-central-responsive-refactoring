426a662784e95d0a19d9de3339d62f20657d75ff
Dennis Kememwa <denniskem6@gmail.com>
2025-07-03 15:23:59 +0300

    worked on db

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
     //
      use HasFactory;
+//one department has many users
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'dep_id', 'id');
+    }
 }
