78709e9b50be1c812d4b1a4f1de248f338b23cc8
Dennis Kememwa <denniskem6@gmail.com>
2025-08-20 17:18:06 +0300

    added breadcrumb for user onboading navigation pages

--- resources/views/livewire/continue-onboarding.blade.php
+++ resources/views/livewire/continue-onboarding.blade.php
@@ -1,5 +1,16 @@
 <div>
 
+<nav class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-700 dark:text-gray-200" aria-label="Breadcrumb">
+    <ol class="inline-flex items-center space-x-2">
+        <li class="inline-flex items-center">
+            <svg class="w-4 h-4 text-blue-600 mr-2" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                <path d="M7.05 5.05a.7.7 0 011 0l4.9 4.9a.7.7 0 010 1l-4.9 4.9a.7.7 0 01-1-1l4.4-4.4-4.4-4.4a.7.7 0 010-1z" />
+            </svg>
+            <span class="text-gray-900 dark:text-white">Hr Continue and complete pending users to be onboarded page</span>
+        </li>
+    </ol>
+</nav>
+
 <div class="flex flex-col md:flex-row gap-4">
     <!-- Card 1 -->
     <div class="max-w-sm md:w-1/3 pb-3">
