de888fae7feddf77793c105edf982e46fd9df1d1
Dennis Kememwa <denniskem6@gmail.com>
2025-08-26 15:14:24 +0300

    added the onboarding table

--- database/migrations/2025_08_25_104832_create_onboardings_table.php
+++ database/migrations/2025_08_25_104832_create_onboardings_table.php
@@ -13,8 +13,16 @@ return new class extends Migration
     {
         Schema::create('onboardings', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable(); // linked after user creation
+            $table->string('email')->unique();
+            $table->string('status')->default('in_progress'); // in_progress | completed
+            $table->json('steps')->nullable();
+            $table->integer('progress')->default(0);
+            $table->boolean('completed')->default(false);
+            $table->timestamp('completed_at')->nullable();
             $table->timestamps();
         });
+
     }
 
     /**
